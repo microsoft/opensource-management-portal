40871495ee2d95fa2b54455bcafa78d9308084c6
Jeff Wilcox <jwilcox@microsoft.com>
2016-06-23 23:10:13 -0700

    Adding HSTS middleware

---
+++ middleware/hsts.js
@@ -0,0 +1,12 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+var hsts = require('hsts');
+
+module.exports = hsts({
+  maxAge: 10886400000,     // Must be at least 18 weeks to be approved
+  includeSubDomains: true, // Must be enabled to be approved
+  preload: true,
+});
