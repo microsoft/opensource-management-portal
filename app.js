09397da729c4506847ecf0a6a9d018b1dfbb2430
Jeff Wilcox <jwilcox@microsoft.com>
2016-07-13 23:56:52 -0700

    Reformatted app.js mildly, plus new config error support

--- app.js
+++ app.js
@@ -32,36 +32,36 @@ app.initializeApplication = function init(config, configurationError, callback) 
   }
 
   var redisFirstCallback;
-    var redisOptions = {
-        auth_pass: config.redis.key,
+  var redisOptions = {
+    auth_pass: config.redis.key,
+  };
+  if (config.redis.tls) {
+    redisOptions.tls = {
+        servername: config.redis.tls,
     };
-    if (config.redis.tls) {
-        redisOptions.tls = {
-            servername: config.redis.tls,
-        };
+  }
+  var redisClient = redis.createClient(config.redis.port, config.redis.host, redisOptions);
+  redisClient.on('connect', function () {
+    if (redisFirstCallback) {
+      var cb = redisFirstCallback;
+      redisFirstCallback = null;
+      cb();
     }
-    var redisClient = redis.createClient(config.redis.port, config.redis.host, redisOptions);
-    redisClient.on('connect', function () {
-        if (redisFirstCallback) {
-            var cb = redisFirstCallback;
-            redisFirstCallback = null;
-            cb();
-        }
     });
     async.parallel([
-        function (cb) {
-            new DataClient(config, function (error, dcInstance) {
-                dc = dcInstance;
-                cb(error);
-            });
-        },
-        function (cb) {
-            redisFirstCallback = cb;
-            redisClient.auth(config.redis.key);
-        },
+      function (cb) {
+        new DataClient(config, function (error, dcInstance) {
+          dc = dcInstance;
+          cb(error);
+        });
+      },
+      function (cb) {
+        redisFirstCallback = cb;
+        redisClient.auth(config.redis.key);
+      },
     ], function (error) {
-        finalizeInitialization(error);
-    });
+    finalizeInitialization(error);
+  });
 };
 
 module.exports = app;
