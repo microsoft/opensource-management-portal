7860b71e320fed81867a944340f9acd1a53447e3
Jeff Wilcox <jwilcox@microsoft.com>
2016-07-28 16:34:17 -0700

    KeyVault missing tags (undefined) allowed, plus KeyVault parse tests

--- test/configuration.js
+++ test/configuration.js
@@ -6,6 +6,8 @@
 'use strict';
 
 const assert = require('chai').assert;
+const keyVaultHelper = require('../lib/keyVaultHelper');
+const fakeKeyVaultClient = require('./fakeKeyVaultClient');
 
 // these constants are defined inside the configuration.js file
 
@@ -37,6 +39,15 @@ const secretConfigurationKeys = [
 ];
 
 // end of borrowed section
+
+function createFakeWithKeys() {
+  const faker = fakeKeyVaultClient();
+  const secretId = faker.storeSecret('test', 'big secret', {
+    tag1: 'p1',
+    tag2: 'and tag 2',
+  });
+  return [faker, secretId];
+}
 
 function wrappingConfigurationHelper(env) {
   return {
@@ -73,7 +84,7 @@ function createBareConfigurationWithScrub() {
 }
 
 describe('configuration', () => {
-  describe('configuration object', () => {
+  describe('config', () => {
     it('initializes some test data', () => {
       const testEnv = createBareMinimumConfiguration();
       const configResult = require('../configuration')(false, testEnv);
@@ -86,13 +97,109 @@ describe('configuration', () => {
       assert.notEqual(configResult.obfuscatedConfig.session.salt, '***', 'session salt is obfuscated');
     });
   });
+
+  describe('keyVaultHelper', () => {
+    it('non-URL values passthrough', () => {
+      const fake = createFakeWithKeys(); // es6 destructuring would be nice
+      const keyVaultClient = fake[0];
+      const config = {
+        a: 'animal',
+        b: 'bat',
+        c: 'cherry',
+        d: true,
+        e: 5,
+      };
+      keyVaultHelper.resolveKeyVaultConfiguration(keyVaultClient, config, (error) => {
+        assert.isNull(error, 'no exception');
+        assert.equal(config.a, 'animal', 'string works');
+        assert.equal(config.b, 'bat', 'string works');
+        assert.equal(config.c, 'cherry', 'string works');
+        assert.isTrue(config.d, 'bool works');
+        assert.equal(config.e, 5, 'number is unaffected');
+      });
+    });
+    it('keyvault:// protocol works', () => {
+      const fake = createFakeWithKeys(); // es6 destructuring would be nice
+      const keyVaultClient = fake[0];
+      const secretId = fake[1];
+      const keyVaultSchemeSecretId = secretId.replace('https://', 'keyvault://');
+      const config = {
+        bigPasscode: keyVaultSchemeSecretId,
+      };
+      keyVaultHelper.resolveKeyVaultConfiguration(keyVaultClient, config, () => {
+        assert.equal(config.bigPasscode, 'big secret', 'secret read OK');
+      });
+    });
+    it('deeply nested KeyVault URLs work', () => {
+      const fake = createFakeWithKeys(); // es6 destructuring would be nice
+      const keyVaultClient = fake[0];
+      const secretId = fake[1];
+      const keyVaultSchemeSecretId = secretId.replace('https://', 'keyvault://');
+      const config = {
+        deep: {
+          object: {
+            nesting: {
+              test: {
+                value: {
+                  is: keyVaultSchemeSecretId,
+                }
+              }
+            }
+          }
+        }
+      };
+      keyVaultHelper.resolveKeyVaultConfiguration(keyVaultClient, config, () => {
+        assert.equal(config.deep.object.nesting.test.value.is, 'big secret', 'secret read OK');
+      });
+    });
+    it('keyvault:// tag properties work', () => {
+      const fake = createFakeWithKeys(); // es6 destructuring would be nice
+      const keyVaultClient = fake[0];
+      const secretId = fake[1];
+      const keyVaultSchemeSecretId = secretId.replace('https://', 'keyvault://');
+      const keyVaultSchemeSecretIdWithTag = secretId.replace('https://', 'keyvault://tag1@');
+      const config = {
+        taggedProperty: keyVaultSchemeSecretIdWithTag,
+        kvProperty: keyVaultSchemeSecretId,
+      };
+      keyVaultHelper.resolveKeyVaultConfiguration(keyVaultClient, config, () => {
+        assert.equal(config.kvProperty, 'big secret', 'secret read OK');
+        assert.equal(config.taggedProperty, 'p1', 'tag read OK');
+      });
+    });
+    it('keyvault:// tag properties return undefined if missing', () => {
+      const fake = createFakeWithKeys(); // es6 destructuring would be nice
+      const keyVaultClient = fake[0];
+      const secretId = fake[1];
+      const keyVaultSchemeSecretIdWithTag = secretId.replace('https://', 'keyvault://undefinedtagthing@');
+      const config = {
+        taggedProperty: keyVaultSchemeSecretIdWithTag,
+      };
+      keyVaultHelper.resolveKeyVaultConfiguration(keyVaultClient, config, () => {
+        assert.isUndefined(config.taggedProperty, '=== undefined');
+      });
+    });
+    it('URL values passthrough', () => {
+      const fake = createFakeWithKeys(); // es6 destructuring would be nice
+      const keyVaultClient = fake[0];
+      const secretId = fake[1];
+      const config = {
+        a: secretId,
+      };
+      keyVaultHelper.resolveKeyVaultConfiguration(keyVaultClient, config, (error) => {
+        assert.isNull(error, 'no exception');
+        assert.equal(config.a, secretId, 'KeyVault URL is passed through');
+      });
+    });
+    it('keyvault:// on an invalid secret stops processing', () => {
+      const fake = createFakeWithKeys(); // es6 destructuring would be nice
+      const keyVaultClient = fake[0];
+      const config = {
+        a: 'keyvault://invalid/secrets/hello/1',
+      };
+      keyVaultHelper.resolveKeyVaultConfiguration(keyVaultClient, config, (error) => {
+        assert.isNotNull(error, 'exception thrown due to KeyVault client error');
+      });
+    });
+  });
 });
-
-
-
-  /*describe('logger', () => {
-    it('default does not share user identifier', () => {
-          //assert.deepEqual(entity, roundtripEntity, 'roundtripEntity is equal to entity');
-    }),
-  });*/
-
