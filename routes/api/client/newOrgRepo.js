7690d2a9d0e80159d6818fa4f36cc183d88e7c71
Jeff Wilcox <jwilcox@microsoft.com>
2017-05-31 10:07:37 -0700

    Route: New experimental API route

---
+++ routes/api/client/newOrgRepo.js
@@ -0,0 +1,246 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+'use strict';
+
+const _ = require('lodash');
+const apiUserContext = require('../apiUserContext');
+const express = require('express');
+const jsonError = require('../jsonError');
+const OpenSourceUser = require('../../../lib/context');
+const router = express.Router();
+
+const createRepo = require('../createRepo');
+
+router.get('/metadata', (req, res, next) => {
+  try {
+    const metadata = req.organization.getRepositoryCreateMetadata();
+    res.json(metadata);
+  } catch (error) {
+    return next(jsonError(error, 400));
+  }
+});
+
+router.get('/personalizedTeams', apiUserContext, (req, res, next) => {
+  const orgName = req.organization.name.toLowerCase();
+  const operations = req.app.settings.providers.operations;
+  const oss = req.oss;
+  const id = oss.id.github;
+  const uc = operations.getUserContext(id);
+  let maintainedTeams = new Set();
+  const broadTeams = new Set(req.organization.broadAccessTeams);
+  uc.getTeamMemberships('maintainer').then(mt => {
+    mt.forEach(maintainedTeam => {
+      maintainedTeams.add(maintainedTeam.id);
+    });
+    return uc.getTeamMemberships();
+  }).then(teams => {
+    _.remove(teams, team => {
+      return team.organization.login.toLowerCase() !== orgName;
+    });
+    teams.forEach(team => {
+      delete team.organization;
+      delete team.slug;
+      team.role = maintainedTeams.has(team.id) ? 'maintainer' : 'member';
+      if (broadTeams.has(team.id)) {
+        team.broad = true;
+      }
+    });
+    return res.json({
+      personalizedTeams: teams,
+    });
+  }).catch(error => {
+    return next(jsonError(error, 400));
+  });
+});
+
+router.get('/teams', apiUserContext, (req, res, next) => {
+  // By default, allow a 30-second old list of teams. If the cached
+  // view is older, refresh this list in the background for use if
+  // they refresh for a better user experience.
+  const caching = {
+    backgroundRefresh: true,
+    maxAgeSeconds: 30,
+  };
+
+  // If the user forces a true refresh, force a true walk of all the teams
+  // from GitHub. This will be slow the larger the org. Allow a short cache
+  // window for the casewhere a  webhook processes the change quickly.
+  if (req.query.refresh) {
+    caching.backgroundRefresh = false;
+    caching.maxAgeSeconds = 10;
+  }
+
+  req.organization.getTeams((getTeamsError, teams) => {
+    if (getTeamsError) {
+      return next(jsonError(getTeamsError), 400);
+    }
+    teams.forEach(teams => {
+      delete teams.otherFields;
+      delete teams.organization;
+      delete teams.slug;
+    });
+    res.json({
+      teams: teams,
+    });
+  });
+});
+
+router.get('/repo/:repo', (req, res) => {
+  const repoName = req.params.repo;
+  req.organization.repository(repoName).getDetails((error) => {
+    res.status(error ? 404 : 204);
+    res.send();
+    req.app.settings.providers.insights.trackEvent('ApiClientNewRepoValidateAvailability', {
+      found: error ? true : false,
+      repoName: repoName,
+      org: req.organization.name,
+    });
+  });
+});
+
+function discoverUserIdentities(req, res, next) {
+  const orgName = req.organization.name;
+  const options = {
+    config: req.app.settings.runtimeConfig,
+    dataClient: req.app.settings.dataclient,
+    redisClient: req.app.settings.dataclient.cleanupInTheFuture.redisClient,
+    redisHelper: req.app.settings.redisHelper,
+    request: req,
+    insights: req.insights,
+    ossDbClient: req.app.settings.providers.ossDbConnection,
+    githubLibrary: req.app.settings.providers.github,
+  };
+  new OpenSourceUser(options, function (error, instance) {
+    req.oss = instance;
+    try {
+      req.org = instance.org(orgName);
+    } catch (ex) {
+      return next(jsonError(ex), 400);
+    }
+    // Try and also learn if we know their e-mail address to send the new repo mail to
+    const upn = instance.modernUser().contactEmail();
+    const mailAddressProvider = req.app.settings.providers.mailAddressProvider;
+    mailAddressProvider.getAddressFromUpn(upn, (resolveError, mailAddress) => {
+      if (!resolveError && mailAddress) {
+        req.knownRequesterMailAddress = mailAddress;
+      }
+      return next();
+    });
+  });
+}
+
+router.post('/repo/:repo', discoverUserIdentities, (req, res, next) => {
+  const body = req.body;
+  if (!body) {
+    return next(jsonError('No body', 400));
+  }
+
+  const config = req.app.settings.runtimeConfig;
+
+  const oss = req.oss;
+  if (oss && oss.usernames) {
+    body['ms.onBehalfOf'] = oss.usernames.github;
+  }
+
+  // Today we always create a private repo if we can
+  // TODO: POST 1ES-DAY: Figure out the proper org config value here
+  body.private = true;
+
+  // these fields do not need translation: name, description
+
+  const approvalTypesToIds = config.github.approvalTypes.fields.approvalTypesToIds;
+  if (!approvalTypesToIds[body.approvalType]) {
+    return next(jsonError('The approval type is not supported or approved at this time', 400));
+  }
+  body.approvalType = approvalTypesToIds[body.approvalType];
+  translateValue(body, 'approvalType', 'ms.approval');
+  translateValue(body, 'approvalUrl', 'ms.approval-url');
+  translateValue(body, 'justification', 'ms.justification');
+  translateValue(body, 'legalEntity', 'ms.cla-entity');
+  translateValue(body, 'claMails', 'ms.cla-mail');
+
+  // Team permissions
+  if (!body.selectedAdminTeams || !body.selectedAdminTeams.length) {
+    return next(jsonError('No administration team(s) provided in the request', 400));
+  }
+  translateTeams(body);
+
+  // Initial repo contents and license
+  const templates = _.keyBy(req.organization.getRepositoryCreateMetadata().templates, 'id');
+  const template = templates[body.template];
+  if (!template) {
+    return next(jsonError('There was a configuration problem, the template metadata was not available for this request', 400));
+  }
+  translateValue(body, 'template', 'ms.template');
+  body['ms.license'] = template.spdx || template.name; // Today this is the "template name" or SPDX if available
+  translateValue(body, 'gitIgnoreTemplate', 'gitignore_template');
+
+  if (!body['ms.notify']) {
+    body['ms.notify'] = req.knownRequesterMailAddress || config.brand.operationsMail || config.brand.supportMail;
+  }
+
+  // these fields are currently ignored:   projectType, orgName, confirmPolicyException
+  delete body.projectType;
+  delete body.orgName;
+  delete body.confirmedPolicyException;
+  delete body.claEntity;
+
+  const token = req.org.setting('ownerToken');
+  createRepo(req, res, body, token, (error, success) => {
+    if (error) {
+      if (!error.json) {
+        error = jsonError(error, 400);
+      }
+      return next(error);
+    }
+
+    success.title = 'Congrats';
+    success.message = success.github ?
+      `Your new repo, ${success.github.name}, has been created and can be found at ${success.github.html_url}.` :
+      'Your repo request has been submitted.';
+
+    // url
+    if (success.github) {
+      success.url = success.github.html_url;
+    }
+
+    // rename tasks to messages
+    success.messages = success.tasks;
+    delete success.tasks;
+
+    res.json(success);
+  }, false /* false means please callback to us with success */);
+});
+
+function translateTeams(body) {
+  let admin = body.selectedAdminTeams;
+  let write = body.selectedWriteTeams;
+  let read = body.selectedReadTeams;
+
+  // Remove teams with higher privileges already
+  _.pullAll(write, admin);
+  _.pullAll(read, admin);
+  _.pullAll(read, write);
+
+  body['ms.teams'] = {
+    admin: admin,
+    push: write,
+    pull: read,
+  };
+
+  delete body.selectedAdminTeams;
+  delete body.selectedWriteTeams;
+  delete body.selectedReadTeams;
+}
+
+function translateValue(object, fromKey, toKey) {
+  if (object[fromKey]) {
+    object[toKey] = object[fromKey];
+    delete object[fromKey];
+  }
+}
+
+module.exports = router;
---
+++ routes/api/index.js
@@ -0,0 +1,140 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+'use strict';
+
+const basicAuth = require('basic-auth');
+const crypto = require('crypto');
+const express = require('express');
+const jsonError = require('./jsonError');
+const router = express.Router();
+
+const apiClient = require('./client');
+const apiPeople = require('./people');
+const apiWebhook = require('./webhook');
+
+const OpenSourceUser = require('../../lib/context');
+
+const createRepo = require('./createRepo');
+
+const hardcodedApiVersions = [
+  '2017-03-08',
+  '2016-12-01',
+];
+
+router.use('/client', apiClient);
+router.use('/webhook', apiWebhook);
+
+// Require a "preview" API version: ?api-version=2016-12-01
+router.use((req, res, next) => {
+  const apiVersion = req.query['api-version'] || req.headers['api-version'];
+  if (!apiVersion) {
+    return next(jsonError('This endpoint requires that an API Version be provided.', 422));
+  }
+  if (apiVersion.toLowerCase() === '2016-09-22_Preview'.toLowerCase()) {
+    return next(jsonError('This endpoint no longer supports the original preview version. Please update your client to use a newer version such as ' + hardcodedApiVersions[0], 422));
+  }
+  if (hardcodedApiVersions.indexOf(apiVersion.toLowerCase()) < 0) {
+    return next(jsonError('This endpoint does not support the API version you provided at this time.', 422));
+  }
+  req.apiVersion = apiVersion;
+  return next();
+});
+
+router.use(function (req, res, next) {
+  const user = basicAuth(req);
+  const key = user? (user.pass || user.name) : null;
+  if (!key) {
+    return next(jsonError('No key supplied', 400));
+  }
+  const sha1 = crypto.createHash('sha1');
+  sha1.update(key);
+  const hashValue = sha1.digest('hex');
+
+  // { owner, description, orgs (comma-sep list) }
+  const dc = req.app.settings.dataclient;
+  const settingType = 'apiKey';
+  const partitionKey = settingType;
+  const rowKey = `${settingType}${hashValue}`;
+  dc.getSetting(partitionKey, rowKey, (error, setting) => {
+    const apiEventProperties = {
+      keyHash: hashValue,
+      apiVersion: req.apiVersion,
+      url: req.originalUrl || req.url,
+    };
+    if (error) {
+      apiEventProperties.failed = true;
+      apiEventProperties.message = error.message;
+      apiEventProperties.statusCode = error.statusCode;
+    }
+    req.insights.trackEvent('ApiRequest', apiEventProperties);
+    if (error) {
+      req.insights.trackMetric('ApiInvalidKey', 1);
+      req.insights.trackException(error);
+      return next(jsonError(error.statusCode === 404 ? 'Key not authorized' : error.message, 401));
+    } else {
+      req.insights.trackMetric('ApiRequest', 1);
+      req.apiKeyRow = setting;
+      next();
+    }
+  });
+});
+
+router.use('/people', apiPeople);
+
+router.use('/:org', function (req, res, next) {
+  const orgName = req.params.org;
+  const apiKeyRow = req.apiKeyRow;
+  if (!apiKeyRow.orgs) {
+    return next(jsonError('There is a problem with the key configuration', 412));
+  }
+  // '*'' is authorized for all organizations in this configuration environment
+  if (apiKeyRow.orgs !== '*') {
+    const orgList = apiKeyRow.orgs.toLowerCase().split(',');
+    if (orgList.indexOf(orgName.toLowerCase()) < 0) {
+      return next(jsonError('The key is not authorized for this organization', 401));
+    }
+  }
+
+  if (!apiKeyRow.apis) {
+    return next(jsonError('The key is not authorized for specific APIs', 401));
+  }
+  const apis = apiKeyRow.apis.split(',');
+  if (apis.indexOf('createRepo') < 0) {
+    return next(jsonError('The key is not authorized to use the repo create APIs', 401));
+  }
+
+  const providers = req.app.settings.providers;
+  const options = {
+    config: req.app.settings.runtimeConfig,
+    dataClient: providers.dataclient,
+    ossDbClient: providers.ossDbConnection,
+    githubLibrary: providers.github,
+  };
+  new OpenSourceUser(options, function (error, instance) {
+    req.oss = instance;
+    var org;
+    try {
+      org = instance.org(orgName);
+    } catch (ex) {
+      return next(jsonError(ex), 400);
+    }
+    req.org = org;
+    return next();
+  });
+});
+
+router.post('/:org/repos', function (req, res, next) {
+  const convergedObject = Object.assign({}, req.headers);
+  req.insights.trackEvent('ApiRepoCreateRequest', convergedObject);
+  Object.assign(convergedObject, req.body);
+  delete convergedObject.access_token;
+  delete convergedObject.authorization;
+
+  const token = req.org.setting('ownerToken');
+  createRepo(req, res, convergedObject, token, next, true /* send the response directly back without the callback */);
+});
+
+module.exports = router;
