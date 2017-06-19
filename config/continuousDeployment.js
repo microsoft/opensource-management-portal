ce0100e8388609276e62a473c6f1f3a29e8a60bc
Jeff Wilcox <jwilcox@microsoft.com>
2017-06-19 13:14:30 -0700

    Layout, config: adding continuous deployment config Helps identify the build information to output in a HTML comment

---
+++ config/continuousDeployment.js
@@ -0,0 +1,15 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+'use strict';
+
+module.exports = graphApi => {
+  // Useful information to help understand which CI/CD pipeline the app came from
+  const packageJson = require('../package.json');
+  const continuousDeployment = packageJson.continuousDeployment || {};
+  continuousDeployment.version = packageJson.version;
+  continuousDeployment.name = packageJson.name;
+  return continuousDeployment;
+};
