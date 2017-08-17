dfb43fb136239bdfb410d17f19d429cf394fe78f
Jeff Wilcox <jwilcox@microsoft.com>
2017-08-17 13:01:24 -0700

    Adding temp job

---
+++ jobs/temp/index.js
@@ -0,0 +1,27 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+/* eslint no-console: ["error", { allow: ["log"] }] */
+
+'use strict';
+
+const moment = require('moment');
+
+const started = moment().utc();
+const startedString = started.format();
+
+const painlessConfigResolver = require('painless-config-resolver')();
+
+painlessConfigResolver.resolve((configurationError, config) => {
+  console.log('configuration resolution attempted');
+
+  if (configurationError) {
+    throw configurationError;
+  }
+
+  console.log('sharing configured information - warning this may contain secrets in the output:');
+  console.dir(config);
+  process.exit(0);
+});
