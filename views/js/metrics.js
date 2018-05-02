b06cf1c2dc43369784083f6065bd729e86bd4d59
Jeff Wilcox <jwilcox@microsoft.com>
2018-05-02 10:44:10 -0700

    Updated views

--- views/js/metrics.js
+++ views/js/metrics.js
@@ -1,12 +1,14 @@
-function displayRepoMetrics(repoId, orgId) {
+function displayRepoMetrics(repoId, orgId, companyName) {
+  $('#metrics').hide();
   $.get('/api/client/metrics/repos/' + repoId, function (repoData) {
     if (!repoData.metrics[0]) {
-      $('#metrics').hide();
       return;
     }
     displayStats(repoData.metrics[0]);
     $.get('/api/client/metrics/orgs/' + orgId, function (orgData) {
-      displayChart(repoData.metrics[0], orgData.metrics[0]);
+      $('#metrics').show();
+      displayResponseTimesChart(repoData.metrics[0], orgData.metrics[0]);
+      displayContributorsChart(repoData.metrics[0], companyName);
     });
   });
 }
@@ -15,6 +17,8 @@ function displayStats(repoData) {
   $('#contributors').text(Number(repoData.Contributors).toLocaleString());
   $('#subscribers').text(Number(repoData.Subscribers).toLocaleString());
   $('#openIssues').text(Number(repoData.OpenIssues).toLocaleString());
+  $('#assignedOpenIssues').text(Number(repoData.AssignedOpenIssues).toLocaleString());
+  $('#unassignedOpenIssues').text(Number(repoData.UnassignedOpenIssues).toLocaleString());
   $('#closedIssues').text(Number(repoData.ClosedIssues).toLocaleString());
   $('#pullRequests').text(Number(repoData.PullRequests).toLocaleString());
   $('#closedPullRequests').text(Number(repoData.ClosedPullRequests).toLocaleString());
@@ -33,7 +37,7 @@ function displayStats(repoData) {
   $('#linesCommittedToday').text(Number(repoData.LinesCommittedToday).toLocaleString());
 }
 
-function displayChart(repoData, orgData) {
+function displayResponseTimesChart(repoData, orgData) {
   var repo = {
     avgDaysToClosePRs: repoData.AvgDaysToClosePullRequests ? repoData.AvgDaysToClosePullRequests.toFixed(1).toLocaleString() : 0,
     avgDaysForFirstResponseToPRs: repoData.AvgDaysForFirstResponseToPullRequests ? repoData.AvgDaysForFirstResponseToPullRequests.toFixed(1).toLocaleString() : 0,
@@ -46,20 +50,21 @@ function displayChart(repoData, orgData) {
     avgDaysToCloseIssues: orgData.AvgDaysToCloseIssues ? orgData.AvgDaysToCloseIssues.toFixed(1).toLocaleString() : 0,
     avgDaysForFirstResponseToIssues: orgData.AvgDaysForFirstResponseToIssues ? orgData.AvgDaysForFirstResponseToIssues.toFixed(1).toLocaleString() : 0
   }
+  var colors = {
+    'This repo': '#0078d7'
+  };
+  colors[orgData.OrgName + ' org'] = '#ffb900'; // Make IE happy
   c3.generate({
-    bindto: '#metricsChart',
+    bindto: '#avgResponsesChart',
     data: {
       x: 'x',
       columns: [
         ['x', 'Avg days to close PRs', 'Avg days for first response to PRs', 'Avg days to close issues', 'Avg days for first response to open issues'],
         ['This repo', repo.avgDaysToClosePRs, repo.avgDaysForFirstResponseToPRs, repo.avgDaysToCloseIssues, repo.avgDaysForFirstResponseToIssues],
-        [orgData.OrgName + ' org', org.avgDaysToClosePRs, org.avgDaysForFirstResponseToPRs, org.avgDaysToCloseIssues, org.avgDaysForFirstResponseToIssues],
+        [orgData.OrgName + ' org', org.avgDaysToClosePRs, org.avgDaysForFirstResponseToPRs, org.avgDaysToCloseIssues, org.avgDaysForFirstResponseToIssues]
       ],
       type: 'bar',
-      colors: {
-        'This repo': '#0078d7',
-        [orgData.OrgName + ' org']: '#ffb900'
-      }
+      colors: colors
     },
     axis: {
       x: {
@@ -72,6 +77,35 @@ function displayChart(repoData, orgData) {
         label: {
           text: 'Days',
           position: 'outermiddle'
+        }
+      }
+    }
+  });
+}
+
+function displayContributorsChart(repoData, companyName) {
+  if (repoData.CompanyContributors === 0 && repoData.CommunityContributors === 0) {
+    $('#contributorsChartTitle').hide();
+    return;
+  }
+  var colors = {
+    'Community contributors': '#107c10'
+  };
+  colors[companyName + ' contributors'] = '#737373'; // Make IE happy
+  c3.generate({
+    bindto: '#contributorsChart',
+    data: {
+      columns: [
+        [companyName + ' contributors', repoData.CompanyContributors],
+        ['Community contributors', repoData.CommunityContributors]
+      ],
+      type: 'pie',
+      colors: colors,
+    },
+    tooltip: {
+      format: {
+        value: function (value, ratio) {
+          return d3.format('%')(ratio) + ' (' + value + ')';
         }
       }
     }
