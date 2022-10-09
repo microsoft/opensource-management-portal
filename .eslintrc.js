ac1e4ac7259698e5006f44e2567a632741002a9e
Nick Schonning <nschonni@gmail.com>
2022-10-09 17:00:57 -0400

    chore: enable @typescript-eslint/no-extra-semi

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,6 @@ module.exports = {
         '@typescript-eslint/no-empty-function': 'off',
         '@typescript-eslint/no-empty-interface': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-extra-semi': 'off',
         '@typescript-eslint/no-inferrable-types': 'off',
         '@typescript-eslint/no-non-null-assertion': 'off',
         '@typescript-eslint/no-this-alias': 'off',
