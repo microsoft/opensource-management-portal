05fc0c28ce8a1bd68f633180b4be729caa228ff9
Nick Schonning <nschonni@gmail.com>
2022-10-09 17:06:03 -0400

    chore: enable @typescript-eslint/explicit-module-boundary-types

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,6 @@ module.exports = {
       extends: ['plugin:@typescript-eslint/recommended'],
       rules: {
         '@typescript-eslint/ban-ts-comment': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-empty-function': 'off',
         '@typescript-eslint/no-empty-interface': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
